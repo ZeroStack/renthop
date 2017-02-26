3297e8ab4a4a8c1d0f21f4ef7370a19563570ef6
ZeroStack <sahilmp8@gmail.com>
2017-02-26 19:53:13 +1100

    load data into R correctly

--- 0_admin.R
+++ 0_admin.R
@@ -1,6 +1,16 @@
 
 
+# Set directory accordingly
+file_dir <- basic:::get_file_dir()
+setwd(file_dir)
 
+# Load packages and data
+packages <- c("jsonlite", "dplyr", "purrr")
+purrr::walk(packages, library, character.only = TRUE, warn.conflicts = FALSE)
 
-setwd(get_file_dir())
+# Load data into R
+data <- fromJSON("raw_data/train.json")
 
+# unlist every variable except `photos` and `features` and convert to tibble
+vars <- setdiff(names(data), c("photos", "features"))
+data <- map_at(data, vars, unlist) %>% tibble::as_tibble(.)
